5dbff8861f097812afed3754408440fe63fffe21
Azimbek <azimb3k@yandex.ru>
2020-06-18 12:48:40 +0600

    Обновлены gitignore, homework.py

--- homework.py
+++ homework.py
@@ -1,20 +1,38 @@
 import time
-
+import os
 import requests
+
+from dotenv import load_dotenv
 from twilio.rest import Client
 
 
+load_dotenv()
+
+
 def get_status(user_id):
-    params = {
-        ...
+    data = {
+        'user_ids': user_id,
+        'v': '5.110',
+        'access_token': os.getenv('access_token_vk'),
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post(
+        'https://api.vk.com/method/users.get', params=data
+        ).json()['response'][0]
+    return response['online']
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    client = Client(
+        os.getenv('account_sid'),
+        os.getenv('auth_token')
+    )
+    message = client.messages.create(
+        to=os.getenv('NUMBER_TO'),
+        from_=os.getenv('NUMBER_FROM'),
+        body=sms_text
+    )
+    return message.sid
 
 
 if __name__ == "__main__":
